7e93a83e103a751d4f9f5f0f73dfe89b9193e0c9
Snow <84701509+snowden421@users.noreply.github.com>
2021-08-25 16:23:40 -0700

    added GET

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require("./data/animals");
 
@@ -103,6 +104,22 @@ app.get("/api/animals/:id", (req, res) => {
     res.send(404);
   }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
